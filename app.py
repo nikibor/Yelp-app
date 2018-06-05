88b464c01e6a41c91bdfd833fe27307ce6df55a6
nikibor <nborgolov96@gmail.com>
2018-06-06 00:52:13 +0400

    v1

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import json
+
+import requests as http_requests
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -16,8 +20,12 @@ def hello_world():
 def test():
     review = request.form['review']
     model = request.form['model']
-    return str(request.form['review'] + request.form['model'])
+
+    data = json.dumps({'text': str(review), 'token': "test"})
+    cnn_response = http_requests.post(url='https://yelp-classification-service.herokuapp.com/api/cnn', json=data)
+    # cnn_response = http_requests.post(url='http://127.0.0.1:5001/api/cnn', json=data)
+    return cnn_response.text
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000)
